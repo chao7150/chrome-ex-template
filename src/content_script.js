1008e512f2698de771e9f39ef0111d4d45c457c3
Satoru Yokoyama <horn051019@yahoo.co.jp>
2018-07-28 00:25:27 +0900

    erase new line

--- src/content_script.js
+++ src/content_script.js
@@ -2,9 +2,7 @@
     const movies = Array.from(document.getElementsByClassName("g_politics"))
     movies.forEach((movie) => movie.style.display="none")
     const rows = Array.from(document.getElementsByClassName("category_row"))
-    rows.forEach((row) => {
-        row.children[2].style.display="none"
-    })
+    rows.forEach((row) => row.children[2].style.display="none")
 }
 erase()
 setInterval(erase, 1000)
